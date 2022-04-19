8e96267c74a5a3e0184a04900379284172219277
AceBurgundy <samadriansabalo99@gmail.com>
2022-04-19 09:43:06 +0800

    Removed animation functions for login and signup (will use css for the animations)

--- static/users.js
+++ static/users.js
@@ -12,43 +12,9 @@ function showHide(jpassword, jeye) {
 }
 
 function loading() {
-    let login = document.getElementById('login');
-    let register = document.getElementById('register');
     let form = document.getElementById('form-register');
 
     form.reset();
-    register.style.visibility = "hidden";
-    login.style.visibility = "visible";
-    login.style.marginRight = "13.2%";
-}
-
-
-function coverToRight() {
-    let register = document.getElementById('register');
-    let cover = document.getElementById('cover');
-    let login = document.getElementById('login');
-
-    login.style.visibility = "hidden";
-    register.style.visibility = "visible";
-    cover.style.transform = "translateX(100%)";
-    register.style.marginLeft = "13%";
-    login.style.marginRight = "7%";
-    login.style.transition = "0.9s";
-}
-
-function coverToLeft() {
-    let register = document.getElementById('register');
-    let cover = document.getElementById('cover');
-    let login = document.getElementById('login');
-
-    register.style.visibility = "hidden";
-    login.style.visibility = "visible";
-    register.style.transition = "0.8s";
-    register.style.marginLeft = "7%";
-    login.style.marginRight = "13.5%";
-    cover.style.transform = "translateX(-1%)";
-    login.style.transition = "1.5s ease-in-out";
-    register.style.transition = "1.7s ease-in-out";
 }
 
 function validate() {
