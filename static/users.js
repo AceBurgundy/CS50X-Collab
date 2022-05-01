309d143f406d8d5d1e3bcf48ff1cb4675d8c542a
AceBurgundy <samadriansabalo99@gmail.com>
2022-05-02 06:29:21 +0800

    Commented out some validation as its no longer used.

--- static/users.js
+++ static/users.js
@@ -2,7 +2,7 @@ const toRegister = document.querySelector("#to-register");
 const toLogin = document.querySelector("#to-login");
 const formsContainer = document.querySelector(".forms-container");
 const cover = document.querySelector(".cover");
-
+ 
 toRegister.addEventListener('click', () => {
     formsContainer.classList.add("to-register");
     cover.classList.add("to-register");
@@ -43,43 +43,22 @@ function loading() {
 }
 
 function validate() {
-    let inner = document.getElementById("password-validate");
+    
     let form = document.getElementById('form-register');
-
-    /* Input Classes */
     const firstName = document.getElementById('first-name').value;
-    const lastName = document.getElementById('last-name').value;
-    const telephone = document.getElementById('telephone').value;
-    let password = document.getElementById("regpassword").value;
-    const confirmPasswordVal = document.getElementById('confirm-password').value;
     const email = document.getElementById('email').value;
-
+    /* Input Classes */
+    console.log(firstName); 
 
     /* Dot Icons */
     const firstNameCircle = document.getElementById('first-name-circle');
-    const lastNameCircle = document.getElementById('last-name-circle');
-    const telephoneCircle = document.getElementById('telephone-circle');
-    const confirmPasswordCircle = document.getElementById('conpass-circle');
+
     const emailCircle = document.getElementById('email-circle');
 
     validations = [
-        (
-            firstName.length > 2 &&
-            firstName.length < 30 &&
-            !firstName.match(/[$&,.[;:=?@#]/) ||
-            firstName === null ?
-
-            firstNameCircle.style.color = "green" : firstNameCircle.style.color = "red"
-        ),
-
-        (
-            lastName.length > 2 &&
-            lastName.length < 30 &&
-            !lastName.match(/[$&,.[;:=?@#]/) ||
-            lastName === null ?
-
-            lastNameCircle.style.color = "green" : lastNameCircle.style.color = "red"
-        ),
+        (firstName.length > 2 ? firstNameCircle.style.color = "green" : firstNameCircle.style.color = "red" ), 
+        (firstName.match(/[A-Za-z]/) ? firstNameCircle.style.color = "green" : firstNameCircle.style.color = "red" ),
+        (firstName.length < 30 ? firstNameCircle.style.color = "green" : firstNameCircle.style.color = "red" ),
 
         (
             email.match(/[@]/) && email.match(".com") ?
@@ -87,11 +66,42 @@ function validate() {
             emailCircle.style.color = "green" : emailCircle.style.color = "red"
         ),
 
+    ]
+
+    console.log(validations); 
+
+    form.addEventListener('submit', (Event) => {
+        if (validations.includes('red')) {
+            Event.preventDefault();
+        } else {
+            return true;
+        }
+    })
+}
+
+
+/*
+ //   const lastName = document.getElementById('last-name').value;
+ //   const telephone = document.getElementById('telephone').value;
+ //    const confirmPasswordVal = document.getElementById('confirm-password').value;]
+ //     let password = document.getElementById("regpassword").value;
+
+ //   const lastNameCircle = document.getElementById('last-name-circle');
+ //   const telephoneCircle = document.getElementById('telephone-circle');
+ //   const confirmPasswordCircle = document.getElementById('conpass-circle');
+
+        let inner = document.getElementById("password-validate");
+        (
+            lastName.length > 2 &&
+            lastName.length < 30 &&
+            firstName.match(/[A-Za-z]/) ||
+            lastName === null ?
+
+            lastNameCircle.style.color = "green" : lastNameCircle.style.color = "red"
         (
             telephone.length > 5 &&
             !telephone.match(/[$&,.[;:=?@#]/) &&
-            !telephone.match(/[A-Z]/) &&
-            !telephone.match(/[a-z]/) ||
+            !telephone.match(/[A-Za-z]/) ||
             telephone === null ?
 
             telephoneCircle.style.color = "green" : telephoneCircle.style.color = "red"
@@ -105,14 +115,5 @@ function validate() {
 
         (password.match(/[$&+,.;:=?@#]/) ? inner.children[3].style.color = "green" : inner.children[3].style.color = "red"),
 
-        (password === confirmPasswordVal ? confirmPasswordCircle.style.color = "green" : confirmPasswordCircle.style.color = "red"),
-    ]
-
-    form.addEventListener('submit', (Event) => {
-        if (validations.includes('red')) {
-            Event.preventDefault();
-        } else {
-            return true;
-        }
-    })
-}
+       (password === confirmPasswordVal ? confirmPasswordCircle.style.color = "green" : confirmPasswordCircle.style.color = "red"),
+*/
