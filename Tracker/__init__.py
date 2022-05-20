09861e091454593f67b7d563ce426fe6aef366d7
AceBurgundy <samadriansabalo99@gmail.com>
2022-05-20 21:11:31 +0800

    Added back code to set session to filesystem and code to ensure responses arent cached. both of these code are from CS50 Finance.py

--- Tracker/__init__.py
+++ Tracker/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_session import Session
 from tempfile import mkdtemp
 
 # Configure application
@@ -15,7 +16,26 @@ app.config['SECRET_KEY'] = 'Adrian2001.'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
+
+"""
+The login manager contains the code that lets your application and Flask-Login work together, 
+such as how to load a user from an ID, where to send users when they need to log in, and the like.
+"""
+
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.html'
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 from Tracker import routes
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
