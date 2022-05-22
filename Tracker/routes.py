0cb97d8db0932f53ee60fcca916bad44beda12b2
AceBurgundy <samadriansabalo99@gmail.com>
2022-05-22 17:05:52 +0800

    Remove register route

--- Tracker/routes.py
+++ Tracker/routes.py
@@ -7,7 +7,7 @@ from Tracker.models import User, Project
 from flask_login import login_user, current_user, login_required, logout_user
 from Tracker.helpers import apology
 
-invalid = ["where","select","update","delete",".schema","from","drop"]
+invalid = ["where","select","update","delete",".schema","from","drop","query"]
 
 #!@$%^&*""_+}{":?></*+[;'./,]-/]
 
@@ -17,7 +17,7 @@ invalid = ["where","select","update","delete",".schema","from","drop"]
 def index():
     """Show all user projects"""
 
-    # projects = User.querry.all(projects)
+    # projects = User.query.all(projects)
 
     return render_template("index.html") #, projects=projects)
 
@@ -45,15 +45,15 @@ def login():
         if emailInput.lower() in invalid:
             return apology("email was not accepted")
 
-        # Ensure input is indeed an emailInput
+        # Ensure input is indeed an email
         if "@" not in emailInput and ".com" not in emailInput:
             return apology("Not an email")
 
-        # Ensure passwordInput was submitted
+        # Ensure password was submitted
         if not passwordInput:
             return apology("you must provide a password")
 
-        # Ensure passwordInput has no invalid inputs
+        # Ensure password has no invalid inputs
         if passwordInput.lower() in invalid:
             return apology("password was not accepted")
 
@@ -66,7 +66,7 @@ def login():
             # Redirect user to home page
             return redirect('/') if next_page else redirect("/")
         except:
-            return apology("Login Unsucessful. Something was wrong on loging in user. line 71")
+            return apology("Login Unsucessful. Something was wrong on logging in user. line 71")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -83,76 +83,7 @@ def logout():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
 
-    logout_user()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        #Check if there is a user currently logged in
-        if current_user.is_authenticated:
-            #if true, redirect them back to the homepage
-            return redirect('/')
-
-        usernameInput = request.form.get("username")
-        emailInput = request.form.get("email")
-        passwordInput = request.form.get("password")
-
-        #ensure that the user placed their username
-        if not usernameInput:
-            return apology("missing username")
-
-        # Ensure username has no invalid inputs
-        if usernameInput.lower() in invalid:
-            return apology("username was not accepted")
-
-        #querries the database for similar usernames
-        #returns a value if found else none
-        has_similar_username = User.query.filter_by(username=usernameInput).first()
-        
-        if has_similar_username:
-            return apology("Username is already taken")
-
-        # Ensure email was submitted
-        if not emailInput:
-            return apology("must provide an email")
-
-        # Ensure input is indeed an email
-        if "@" not in emailInput and ".com" not in emailInput:
-            return apology("Not an email")
-
-        #querries the database for similar email
-        #returns a value if found else none
-        has_similar_email = User.query.filter_by(email=emailInput).first()
-        
-        if has_similar_email:
-            return apology("You can only use the same email once")
-
-        # Ensure password was submitted
-        if not passwordInput:
-            return apology("must provide password")
-
-        # Ensure password has no invalid inputs
-        if passwordInput.lower() in invalid:
-            return apology("password was not accepted")
-
-        encrypted_password = generate_password_hash(passwordInput)
-
-        # Add new user to the database
-        try:
-            user = User(username=usernameInput, email=emailInput, password=encrypted_password)
-            db.session.add(user)
-            db.session.commit()
-            flash(f'Succesfully created account') #,success')
-            return redirect("/")
-            # Redirect user to home page
-        except:
-            return apology("Something went wrong when trying to push to the database")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("users.html")
 
 # #Add project
 
