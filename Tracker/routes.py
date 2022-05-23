e26f667be39d5920858fbd65a502840948e55b5c
AceBurgundy <96225310+AceBurgundy@users.noreply.github.com>
2022-05-23 11:10:00 +0800

    Update routes.py
    Fixed routes.py login route

--- Tracker/routes.py
+++ Tracker/routes.py
@@ -23,11 +23,11 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
+      """Log user in"""
 
     logout_user()
 
-   # User reached route via POST (as by submitting a form via POST)
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
         #ensure that there is no user that is currently logged in
@@ -36,9 +36,8 @@ def login():
 
         emailInput = request.form.get("email")
         passwordInput = request.form.get("password")
-        usernameInput = request.form.get("username")
 
-         # Ensure that the user placed their email
+        # Ensure that the user placed their email
         if not emailInput:
             return apology("missing email")
 
@@ -50,13 +49,6 @@ def login():
         if "@" not in emailInput and ".com" not in emailInput:
             return apology("Not an email")
 
-        # Ensure email has been already used 
-        has_similar_email = User.query.filter_by(email=emailInput).first()
-
-        if has_similar_email: 
-            return apology("You have already used this email. Try another one")
-
-
         # Ensure password was submitted
         if not passwordInput:
             return apology("you must provide a password")
@@ -65,25 +57,14 @@ def login():
         if passwordInput.lower() in invalid:
             return apology("password was not accepted")
 
-        # Ensure user inputs username
-        if not usernameInput: 
-            return apology("Please provide a username")
-        
-        # Ensure username has no invalid inputs
-        if usernameInput.lower() in invalid:
-            return apology("Invalid username, please try again")
-
-        encryptedPassword = generate_password_hash(passwordInput)
-
         try:
-            user = User(username = usernameInput, email = emailInput, password = encryptedPassword)
+            user = User.query.filter_by(email=emailInput).first()
             
-            db.session.add(user)
-            db.session.commit()
-            flash(f"Successfully created account.") #, success 
-
+            if user and check_password_hash(user.password, passwordInput):
+                login_user(user) #, remember=form.remember.data)
+                next_page = request.args.get('next')
             # Redirect user to home page
-            return redirect('/')
+            return redirect('/') if next_page else redirect("/")
         except:
             return apology("Login Unsucessful. Something was wrong on logging in user. line 71")
 
