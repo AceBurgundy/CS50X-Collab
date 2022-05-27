59c206424becd8d224cf9bc52938d4686b58816a
AceBurgundy <samadriansabalo99@gmail.com>
2022-05-27 14:22:00 +0800

    Small change on indentions

--- Tracker/routes.py
+++ Tracker/routes.py
@@ -23,9 +23,9 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-      """Log user in"""
+    """Log user in"""
 
-logout_user()
+    logout_user()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
