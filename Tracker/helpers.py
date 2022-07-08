d106f76346cf9096c17fdf5e332c0bc98c49f4c5
AceBurgundy <samadriansabalo99@gmail.com>
2022-07-08 14:52:56 +0800

    removed apology function code
    added save_picture function to crop photos

--- Tracker/helpers.py
+++ Tracker/helpers.py
@@ -1,19 +1,15 @@
 
+from operator import indexOf
+import os
+import secrets
+from turtle import width
 from flask import render_template
+from Tracker import app
+from PIL import Image
 
 def apology(message, code=400):
     """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
+    return render_template("apology.html", top=code, bottom=message), code
 
 def Search(list,search):   
     try:        
@@ -24,16 +20,25 @@ def Search(list,search):
             return False
 
 def save_picture(form_picture):
+    i = Image.open(form_picture)
+    
+    width, height = i.size
+    print(width,height)
     random_value = secrets.token_hex(8)
     # fileName = "_"
     _, fileExtension = os.path.splitext(form_picture.filename)
     profilePictureFileName = random_value + fileExtension
     picture_path = os.path.join(app.root_path, 'static/profile_pictures', profilePictureFileName)
-
     outputSize = (125,125)
     
-    i = Image.open(form_picture)
-    i.thumbnail(outputSize)
-    i.save(picture_path)
-        
+    # cropped = i.crop((500,100,500,500))
+
+    if width > height:
+        cropped = i.crop((400,100,1470,1100))
+    else:
+        cropped = i.crop((0,-(width/2),height,height)) #fix this shit tomorrow
+    
+    cropped.thumbnail(outputSize)
+    cropped.save(picture_path)
+    
     return profilePictureFileName
