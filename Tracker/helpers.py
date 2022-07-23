269f5438574d66204f6c2e4a37aef9c4004f716c
AceBurgundy <samadriansabalo99@gmail.com>
2022-07-23 18:43:27 +0800

    removed function search and reorganized save_picture function to just lower the quality of the photos

--- Tracker/helpers.py
+++ Tracker/helpers.py
@@ -1,8 +1,6 @@
 
-from operator import indexOf
 import os
 import secrets
-from turtle import width
 from flask import render_template
 from Tracker import app
 from PIL import Image
@@ -11,34 +9,15 @@ def apology(message, code=400):
     """Render message as an apology to user."""
     return render_template("apology.html", top=code, bottom=message), code
 
-def Search(list,search):   
-    try:        
-        index = indexOf(list, search)
-        if index:
-            return True
-    except:
-            return False
-
 def save_picture(form_picture):
-    i = Image.open(form_picture)
-    
-    width, height = i.size
-    print(width,height)
     random_value = secrets.token_hex(8)
-    # fileName = "_"
-    _, fileExtension = os.path.splitext(form_picture.filename)
-    profilePictureFileName = random_value + fileExtension
-    picture_path = os.path.join(app.root_path, 'static/profile_pictures', profilePictureFileName)
-    outputSize = (125,125)
+    file_name, file_extension = os.path.splitext(form_picture.filename)
+    new_file_name = random_value + file_extension
+    file_path = os.path.join(app.root_path, 'static/profile_pictures', new_file_name)
     
-    # cropped = i.crop((500,100,500,500))
-
-    if width > height:
-        cropped = i.crop((400,100,1470,1100))
-    else:
-        cropped = i.crop((0,-(width/2),height,height)) #fix this shit tomorrow
+    output_size = (125,125)
+    image = Image.open(form_picture)
+    image.thumbnail(output_size)
+    image.save(file_path)
     
-    cropped.thumbnail(outputSize)
-    cropped.save(picture_path)
-    
-    return profilePictureFileName
+    return new_file_name
