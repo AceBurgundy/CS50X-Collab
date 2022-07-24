d190e92f08d99beae7da34f8805a20ddfd20df3f
AceBurgundy <samadriansabalo99@gmail.com>
2022-07-24 08:27:06 +0800

    Removed all comments at the top of helper.js file and added a much more simpler explanation inside the function. Also added function to freely toggle eye icons to close and open.

--- Tracker/static/helper.js
+++ Tracker/static/helper.js
@@ -1,30 +1,42 @@
-// A function that counts the length of the input and changes the value of a span element 0/value
-
-/* 
- Ex: 
- 
-    Input: Lorem ipsum
-
-    Counter: 11/value
-
-    For this to work, add this to your input
-
-    counter(id-of-input, id-of-counter, limit-of-input)
-
-    your html elements should look like this,
-
-    <p id="id-of-counter"><span>0</span>/ (limit-of-input) </p>
-    <input type="text" id="id-of-input" onkeyup="counter('id-of-input','id-of-counter',limit-of-input)" maxlength="30">
- 
-*/
-
 // use default only on one function or class
-export function counter(inputId, counterId, restrict) {
+export function counter(inputId, counterId, restriction) {
+    /* Add function to any character counter you add in javascript
 
-    const limit = restrict
+        inputId: the id of the input
+        counterId: the id of the number which should change depending on the count
+        restriction: <integer> adds a limit
+    */
+    inputId.addEventListener('input', () => {
+        if (restriction === null) {
+            restriction = counterId.children[0].textContent = inputId.value.length
+        } else if (inputId.value.length <= restriction) {
+            counterId.children[0].textContent = inputId.value.length
+        }
+    })
 
-    inputId.addEventListener('keyup', () => {
+    window.addEventListener("load", () => {
+        counterId.children[0].textContent = inputId.value.length
+    })
+}
 
-        inputId.value.length <= limit ? counterId.children[0].textContent = inputId.value.length : counterId.style.color = "red";
+export function eyeToggle(eyesContainerId, inputId, eyeId, eyeSlashId) {
+    /* Add function to open and close eye icons in forms 
+    
+    eyesContainerId: container where all your eye icons are placed
+    inputId: the id of the input
+    eyeId: the id of the eye icon (open)
+    eyeSlashId: the id of the eye icon (close)
+    
+    */
+    eyesContainerId.addEventListener("click", () => {
+        if (inputId.type == "text") {
+            inputId.setAttribute("type", "password")
+            eyeId.style.display = "none"
+            eyeSlashId.style.display = "block"
+        } else {
+            inputId.setAttribute("type", "text")
+            eyeId.style.display = "block"
+            eyeSlashId.style.display = "none"
+        }
     })
 }
