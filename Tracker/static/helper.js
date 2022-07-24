c3d340f3a5fadf36891b1308744d23201ef8d5ff
AACaps <angeloaugustine.capili@gmail.com>
2022-07-24 19:03:04 +0800

    created again the static folder which contains all static files needed by all packages

---
+++ Tracker/static/helper.js
@@ -0,0 +1,42 @@
+// use default only on one function or class
+export function counter(inputId, counterId, restriction) {
+    /* Add function to any character counter you add in javascript
+
+        inputId: the id of the input
+        counterId: the id of the number which should change depending on the count
+        restriction: <integer> adds a limit
+    */
+    inputId.addEventListener('input', () => {
+        if (restriction === null) {
+            restriction = counterId.children[0].textContent = inputId.value.length
+        } else if (inputId.value.length <= restriction) {
+            counterId.children[0].textContent = inputId.value.length
+        }
+    })
+
+    window.addEventListener("load", () => {
+        counterId.children[0].textContent = inputId.value.length
+    })
+}
+
+export function eyeToggle(eyesContainerId, inputId, eyeId, eyeSlashId) {
+    /* Add function to open and close eye icons in forms 
+    
+    eyesContainerId: container where all your eye icons are placed
+    inputId: the id of the input
+    eyeId: the id of the eye icon (open)
+    eyeSlashId: the id of the eye icon (close)
+    
+    */
+    eyesContainerId.addEventListener("click", () => {
+        if (inputId.type == "text") {
+            inputId.setAttribute("type", "password")
+            eyeId.style.display = "none"
+            eyeSlashId.style.display = "block"
+        } else {
+            inputId.setAttribute("type", "text")
+            eyeId.style.display = "block"
+            eyeSlashId.style.display = "none"
+        }
+    })
+}
