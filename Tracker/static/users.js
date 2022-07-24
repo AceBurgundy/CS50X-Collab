89e61bb83d69849f6a3d9ff9a6dc8bb8ee488949
AceBurgundy <samadriansabalo99@gmail.com>
2022-07-24 08:31:17 +0800

    removed all functions unrelated to users and removed logic to open and close eye icons

--- Tracker/static/users.js
+++ Tracker/static/users.js
@@ -1,8 +1,9 @@
+import { eyeToggle } from "./helper";
 const toRegister = document.querySelector("#to-register");
 const toLogin = document.querySelector("#to-login");
 const formsContainer = document.querySelector(".forms-container");
 const cover = document.querySelector(".cover");
- 
+
 toRegister.addEventListener('click', () => {
     formsContainer.classList.add("to-register");
     cover.classList.add("to-register");
@@ -13,29 +14,22 @@ toLogin.addEventListener('click', () => {
     cover.classList.remove("to-register");
 })
 
-function showHide(password, eye, eyeSlash) {
-    let eyeClose = document.getElementById(eyeSlash);
-    let eyeOpen = document.getElementById(eye);
-    let Inputpassword = document.getElementById(password);
 
-    if (Inputpassword.type == "text") {
-        Inputpassword.setAttribute("type", "password");
-        eyeOpen.style.display = "none";
-        eyeClose.style.display = "block";
-    } else {
-        Inputpassword.setAttribute("type", "text");
-        eyeOpen.style.display = "block";
-        eyeClose.style.display = "none";
-    }
-}
+eyeToggle(
+    document.getElementById("regpassword-icon-container"),
+    document.getElementById("regpassword"),
+    document.getElementById("regeye"),
+    document.getElementById("reg-eye-off")
+)
 
-window.onload = function() {
-    document.getElementById('username').value = "AACaps";
-    document.getElementById('email').value = "aacaps@gmail.com";
-    document.getElementById('regpassword').value = "aac12345";
-}
+eyeToggle(
+    document.getElementById('logpassword-icon-container'),
+    document.getElementById('logpassword'),
+    document.getElementById('logeye'),
+    document.getElementById('log-eye-off')
+)
 
-function loading() {
+window.onload = function loading() {
     var cover = document.querySelector('.cover');
     var formsContainer = document.querySelector('.forms-container');
     cover.addEventListener('touchmove', preventKeyBoardScroll, { passive: false });
@@ -51,8 +45,8 @@ function loading() {
     form.reset();
 }
 
-function validate() {
-    
+document.querySelector("#form-register").addEventListener(("keyup"), () => {
+
     let form = document.getElementById('form-register');
     const userName = document.getElementById('username').value;
     const regPassword = document.getElementById('regpassword');
@@ -64,11 +58,11 @@ function validate() {
     const emailCircle = document.getElementById('email-circle');
 
     validations = [
-        (userName.length > 2 ? userNameCircle.style.fill = "green" : userNameCircle.style.fill = "red" ),
-        (userName.length < 30 ? userNameCircle.style.fill = "green" : userNameCircle.style.fill = "red" ),
-        (userName.match(pattern) || userName.trim() === "" ? userNameCircle.style.fill = "red" : userNameCircle.style.fill = "green" ),
+        (userName.length > 2 ? userNameCircle.style.fill = "green" : userNameCircle.style.fill = "red"),
+        (userName.length < 30 ? userNameCircle.style.fill = "green" : userNameCircle.style.fill = "red"),
+        (userName.match(pattern) || userName.trim() === "" ? userNameCircle.style.fill = "red" : userNameCircle.style.fill = "green"),
         (email.match(/[@]/) && email.match(".com") ? emailCircle.style.fill = "green" : emailCircle.style.fill = "red"),
-        (regPassword.value.match(/(where)|(select)|(update)|(delete)|(.schema)|(from)|(drop)|-/gi) ? regPassword.style.color = "red" : regPassword.style.color = "green") 
+        (regPassword.value.match(/(where)|(select)|(update)|(delete)|(.schema)|(from)|(drop)|-/gi) ? regPassword.style.color = "red" : regPassword.style.color = "green")
     ]
 
     form.addEventListener('submit', (Event) => {
@@ -78,4 +72,4 @@ function validate() {
             return true;
         }
     })
-}
+})
