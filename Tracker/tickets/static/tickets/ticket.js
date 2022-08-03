0ffe145438fb358943a0b33531c4aea25790ece9
Aceburgundy <Samadriansabalo99@gmail.com>
2022-08-03 16:09:42 +0800

    these are created directly from flask

---
+++ Tracker/tickets/static/tickets/ticket.js
@@ -0,0 +1,35 @@
+// let ticket = dragElement.querySelectorAll(".ticket");
+// let eachColumn = document.querySelectorAll(".ticket-column-body")
+
+// ticket.forEach(ticket => {
+//     ticket.addEventListener("dragstart", dragStart)
+//     ticket.addEventListener("dragend", dragEnd)
+// })
+// for (const body of eachColumn) {
+//     body.addEventListener("dragover", console.log("dragover"))
+//     body.addEventListener("dragenter", console.log("dragenter"))
+//     body.addEventListener("dragover", console.log("dragover"))
+//     body.addEventListener("drop", console.log("drop"))
+// }
+
+// function dragStart() {
+//     console.log("Start");
+// }
+
+// function dragEnd() {
+//     console.log("End");
+// }
+
+// // function dragOver() {
+
+// // }
+
+// // function dragEnter() {
+
+// // }
+
+// // function dragLeave() {
+
+// // }
+
+// length of last string
