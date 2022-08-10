86d2449fc3164e2e6b935402dde9c47bba040697
AACaps <angeloaugustine.capili@gmail.com>
2022-08-10 21:40:03 +0800

    Removed unused imports.

--- Tracker/projects/forms.py
+++ Tracker/projects/forms.py
@@ -1,13 +1,7 @@
 from flask_wtf import FlaskForm
-from Tracker import db, database
-import operator
 from wtforms import StringField, SubmitField, TextAreaField, DateField, HiddenField
-from wtforms_sqlalchemy.fields import QuerySelectMultipleField
 from wtforms.validators import DataRequired, Length
 from Tracker.helpers import CheckProfanity
-from Tracker.models import User
-from sqlalchemy import select, text
-
 class ProjectForm(FlaskForm):
     title = StringField(u'title', id="form-title", validators=[DataRequired(message="Your project needs a title"), Length(min=5,max=50), CheckProfanity(), Length(max=50)])
     
