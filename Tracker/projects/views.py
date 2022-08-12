16f1582f9c0dc51cd13c8e7369750cac01e14222
AceBurgundy <Samadriansabalo99@gmail.com>
2022-08-12 13:48:37 +0800

    imported jsonify

--- Tracker/projects/views.py
+++ Tracker/projects/views.py
@@ -1,6 +1,6 @@
 from Tracker import db
 from Tracker.models import Project, User
-from flask import redirect, render_template, request, url_for, flash, Blueprint
+from flask import redirect, render_template, jsonify, request, url_for, flash, Blueprint
 from flask_login import current_user, login_required
 from Tracker.projects.forms import ProjectForm
 from sqlalchemy import update, select
@@ -44,13 +44,11 @@ def update_project(current_project_id):
     modal_title = "UPDATE THIS PROJECT"
     form = ProjectForm()
     project = Project.query.filter_by(id=current_project_id)
-    # project_collaborators = database.execute("SELECT username FROM User WHERE project_id = ?", current_project_id)
     users = db.session.scalars(select(User)).all()
     # unfixed
     if request.method == "POST":
         if form.validate_on_submit():
             try:
-                             
                 db.session.execute(update(Project).where(Project.id==current_project_id).values(
                     title=form.title.data,
                     description=form.description.data,
@@ -60,9 +58,10 @@ def update_project(current_project_id):
                 
                 if form.collaborate.data is not None:
                     for user in form.collaborate.data:
-                        user.collaborated_projects.append(current_project_id)
+                        if user not in project.collaborators:
+                            user.collaborated_projects.append(current_project_id)
+                    print(project.collaborators)
                     db.session.commit()
-
                 flash('Project updated')
                 return redirect(url_for('index._index'))
             except:
@@ -76,6 +75,34 @@ def update_project(current_project_id):
         form.description.data = project.description
         form.deadline.data = project.deadline
         return render_template('project-modal.html', project_collaborators=project.collaborators, users=users, project_id=current_project_id, modal_title=modal_title, form=form)
+
+@projects.route("/remove-collaborator", methods=["POST"])
+@login_required
+def remove_user():
+
+    if request.method == "POST":
+        
+        project = Project.query.filter_by(id=request.args.get("current_project_id", type=int))
+
+        user = User.query.filter_by(id=request.args.get("user_id", type=int))
+
+        project.collaborators.remove(user)
+        
+    return jsonify(f'result=`{user.username}` removed', 200)
+
+@projects.route("/leave_project", methods=["POST"])
+@login_required
+def leave_project():
+
+    if request.method == "POST":
+        
+        project = Project.query.filter_by(id=request.args.get("current_project_id", type=int))
+
+        user = User.query.filter_by(id=request.args.get("user_id", type=int))
+
+        project.collaborators.remove(user)
+        
+    return jsonify(f'result=`{user.username}` had left the project', 200)
 
 # Bookmark/Unbookmark a project
 
