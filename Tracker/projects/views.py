d70a8e25cc1c9150ce54d2717eb9863c32877b83
AceBurgundy <Samadriansabalo99@gmail.com>
2022-08-17 11:00:11 +0800

    Updated projects views.

--- Tracker/projects/views.py
+++ Tracker/projects/views.py
@@ -1,3 +1,4 @@
+from email import message
 from Tracker import db
 from Tracker.models import Project, User
 from flask import redirect, render_template, jsonify, request, url_for, flash, Blueprint
@@ -5,9 +6,11 @@ from flask_login import current_user, login_required
 from Tracker.projects.forms import ProjectForm
 from sqlalchemy import update, select
 
-projects = Blueprint('projects', __name__, template_folder='templates/projects', static_folder='static/projects')
+projects = Blueprint('projects', __name__,
+                     template_folder='templates/projects', static_folder='static/projects')
 
-@projects.route("/add-project", methods=["POST","GET"])
+
+@projects.route("/add-project", methods=["POST", "GET"])
 @login_required
 def add_project():
 
@@ -19,13 +22,13 @@ def add_project():
 
             try:
                 db.session.add(Project(
-                    title = form.title.data, 
-                    description = form.description.data, 
-                    deadline = form.deadline.data, 
+                    title=form.title.data,
+                    description=form.description.data,
+                    deadline=form.deadline.data,
                     author=current_user))
-            
+
                 db.session.commit()
-                
+
                 flash('Project added')
                 return redirect(url_for('index._index'))
             except:
@@ -35,76 +38,75 @@ def add_project():
             return render_template('project-modal.html', users=users, modal_title=modal_title, form=form, error=form.errors)
     else:
         return render_template('project-modal.html', users=users, modal_title=modal_title, form=form)
-    
-@projects.route("/update-project/<int:current_project_id>", methods=["POST","GET"])
+
+
+@projects.route("/update-project/<int:current_project_id>", methods=["POST", "GET"])
 @login_required
 def update_project(current_project_id):
     """ Update project """
 
     modal_title = "UPDATE THIS PROJECT"
     form = ProjectForm()
-    project = Project.query.filter_by(id=current_project_id)
+    project = Project.query.get(current_project_id)
     users = db.session.scalars(select(User)).all()
     # unfixed
     if request.method == "POST":
         if form.validate_on_submit():
             try:
-                db.session.execute(update(Project).where(Project.id==current_project_id).values(
+                db.session.execute(update(Project).where(Project.id == current_project_id).values(
                     title=form.title.data,
                     description=form.description.data,
                     deadline=form.deadline.data,
                     user_id=current_user.id))
                 db.session.commit()
-                
+
                 if form.collaborate.data is not None:
-                    for user in form.collaborate.data:
-                        if user not in project.collaborators:
-                            user.collaborated_projects.append(current_project_id)
-                    print(project.collaborators)
-                    db.session.commit()
+                    collaborate_input = "".join(
+                        form.collaborate.data).split(",")
+
+                for user in users:
+                    if str(user.username) in collaborate_input:
+                        user.collaborated_projects.append(project)
+                db.session.commit()
+
                 flash('Project updated')
                 return redirect(url_for('index._index'))
             except:
                 form.form_errors.append('Failed to update project ')
-                return render_template('project-modal.html', project_collaborators=project.collaborators, users=users, project_id=current_project_id, modal_title=modal_title, form=form, error=form.errors)
+                return render_template('project-modal.html', users=users, project_id=current_project_id, modal_title=modal_title, form=form, error=form.errors)
         else:
-            return render_template('project-modal.html', project_collaborators=project.collaborators, users=users, project_id=current_project_id, modal_title=modal_title, form=form, error=form.errors)
+            return render_template('project-modal.html', users=users, project_id=current_project_id, modal_title=modal_title, form=form, error=form.errors)
     else:
-        project = Project.query.filter_by(id=current_project_id).first() 
-        form.title.data = project.title 
+        project = Project.query.filter_by(id=current_project_id).first()
+        form.title.data = project.title
         form.description.data = project.description
         form.deadline.data = project.deadline
-        return render_template('project-modal.html', project_collaborators=project.collaborators, users=users, project_id=current_project_id, modal_title=modal_title, form=form)
+        # project_collaborators=project.collaborators
+        return render_template('project-modal.html', users=users, project_id=current_project_id, modal_title=modal_title, form=form)
+
 
 @projects.route("/remove-collaborator", methods=["POST"])
 @login_required
 def remove_user():
 
     if request.method == "POST":
-        
-        project = Project.query.filter_by(id=request.args.get("current_project_id", type=int))
 
-        user = User.query.filter_by(id=request.args.get("user_id", type=int))
+        project = Project.query.get(
+            request.form["current_project_id"])
 
-        project.collaborators.remove(user)
-        
-    return jsonify(f'result=`{user.username}` removed', 200)
+        user = User.query.get(request.form["user_id"])
 
-@projects.route("/leave_project", methods=["POST"])
-@login_required
-def leave_project():
+        user.collaborated_projects.remove(project)
+        db.session.commit()
 
-    if request.method == "POST":
-        
-        project = Project.query.filter_by(id=request.args.get("current_project_id", type=int))
-
-        user = User.query.filter_by(id=request.args.get("user_id", type=int))
-
-        project.collaborators.remove(user)
-        
-    return jsonify(f'result=`{user.username}` had left the project', 200)
+        if request.form["leave"] == 'leave':
+            flash(f'You left the {project.title} project')
+        else:
+            flash(f'{user.username} had been removed from the project')
+    return redirect(url_for('index._index'))
 
 # Bookmark/Unbookmark a project
+
 
 @projects.route("/bookmark/<int:current_project_id>", methods=["POST"])
 @login_required
@@ -116,13 +118,15 @@ def bookmark_project(current_project_id):
         print(project_state.bookmark_state)
         try:
             if project_state.bookmark_state == False:
-                db.session.execute(update(Project).where(Project.id==current_project_id).values(bookmark_state=True))
+                db.session.execute(update(Project).where(
+                    Project.id == current_project_id).values(bookmark_state=True))
                 db.session.commit()
                 flash('Project bookmarked')
                 return redirect(url_for('index._index'))
-                
+
             if project_state.bookmark_state == True:
-                db.session.execute(update(Project).where(Project.id==current_project_id).values(bookmark_state=False))
+                db.session.execute(update(Project).where(
+                    Project.id == current_project_id).values(bookmark_state=False))
                 db.session.commit()
                 flash('Project bookmark removed')
                 return redirect(url_for('index._index'))
@@ -132,13 +136,14 @@ def bookmark_project(current_project_id):
 
 # Delete project
 
+
 @projects.route("/delete/<int:current_project_id>", methods=["POST"])
 @login_required
 def delete_project(current_project_id):
     """ Delete project """
 
     if request.method == "POST":
-        try:      
+        try:
             db.session.delete(Project.query.get(current_project_id))
             db.session.commit()
             flash('Project deleted')
