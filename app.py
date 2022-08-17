16ec9490d41803dc4d34d181b735d1d74d7a8b25
AceBurgundy <Samadriansabalo99@gmail.com>
2022-08-17 10:58:52 +0800

    changed app.run to run on port 8080.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from Tracker import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
 
     # if you are seeing this problem;
     # Error: Could not locate a Flask application. You did not provide the "FLASK_APP" environment variable, and a "wsgi.py" or "app.py" module was not found in the current directory.
