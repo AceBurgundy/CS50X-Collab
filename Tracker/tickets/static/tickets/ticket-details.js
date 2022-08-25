86ade703d010ff691ca3a36b3880f65fa6611071
AceBurgundy <Samadriansabalo99@gmail.com>
2022-08-25 09:33:50 +0800

    Added script for clicking edit, save and other comment hover interactions.

--- Tracker/tickets/static/tickets/ticket-details.js
+++ Tracker/tickets/static/tickets/ticket-details.js
@@ -17,3 +17,43 @@ document.querySelectorAll('.ticket-tag-container').forEach(ticket => {
         ticket.children[0]
     }
 })
+
+const commentBox = document.querySelector('.author-comment-message')
+const commentBoxOptions = document.querySelector('.add-comment-options-container')
+
+commentBox.addEventListener("click", () => {
+    commentBoxOptions.style.display = 'flex'
+})
+
+document.querySelector('.cancel-comment').addEventListener("click", (e) => {
+    e.preventDefault()
+    commentBoxOptions.style.display = 'none'
+})
+
+document.querySelectorAll('.comment').forEach(comment => {
+    comment.classList.add("hoverable")
+})
+
+document.querySelectorAll('.edit-comment').forEach(editButton => {
+    editButton.addEventListener("click", () => {
+        editButton.parentElement.parentElement.parentElement.classList.remove("hoverable")
+        editButton.parentElement.parentElement.previousElementSibling.children[1].firstElementChild.children[1].children[1].style.display = 'flex'
+        editButton.parentElement.parentElement.previousElementSibling.children[1].firstElementChild.firstElementChild.classList.add("active")
+    })
+})
+
+document.querySelectorAll('.cancel-comment-edit').forEach(cancelButton => {
+    cancelButton.addEventListener("click", () => {
+        cancelButton.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.add("hoverable")
+        cancelButton.parentElement.style.display = 'none'
+        cancelButton.parentElement.parentElement.previousElementSibling.classList.remove("active")
+    })
+})
+
+commentBox.addEventListener('input', autoResize, false);
+
+document.querySelectorAll('.menu-icon').forEach(commentOption => {
+    commentOption.addEventListener("mouseover", () => {
+        commentOption.nextElementSibling.classList.toggle("active")
+    })
+})
