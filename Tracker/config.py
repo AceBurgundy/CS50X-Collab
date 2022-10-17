6c1f1a593d4080f378569cacf00c1005005af301
AceBurgundy <Samadriansabalo99@gmail.com>
2022-10-17 11:26:12 +0800

    Minor changes.

--- Tracker/config.py
+++ Tracker/config.py
@@ -2,7 +2,7 @@ class Config:
         # Ensure templates are auto-reloaded
     TEMPLATES_AUTO_RELOAD = True
 
-    SECRET_KEY = 'Adrian2001.'
+    SECRET_KEY = 'Blackasdasdasdwq324535jf34f34f.'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///collab.db'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
